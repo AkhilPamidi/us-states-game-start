44f213b58b66cee864fff147581308e1f67f4aeb
coderockzz <91675151+coderockzz@users.noreply.github.com>
2021-11-19 20:26:53 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+from turtle import Turtle
+import pandas
+import turtle
+
+screen = turtle.Screen()
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+data = pandas.read_csv("50_states.csv")
+length = len(data.state)
+all_states = data.state.tolist()
+you_guessed = 0
+lives = 5
+answers=[]
+while lives > 0:
+    answer = screen.textinput(title="guess the state", prompt=f"your total guess is {you_guessed}/50").title()
+    if answer in all_states:
+        if answer not in answers:
+            check = data[data.state == answer]
+            new = Turtle("circle")
+            new.shapesize(0.2)
+            new.penup()
+            new.color("black")
+            new.goto(x=float(check.x), y=float(check.y))
+            new.write(answer)
+            answers.append(answer)
+            you_guessed += 1
+    else:
+        lives -= 1
+screen.exitonclick()
