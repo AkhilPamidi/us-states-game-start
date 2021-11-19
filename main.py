430bdfe85d79a06a0e0cda25e30c62265942ca6c
coderockzz <91675151+coderockzz@users.noreply.github.com>
2021-11-19 20:46:44 +0530

    sec version

--- main.py
+++ main.py
@@ -11,20 +11,25 @@ length = len(data.state)
 all_states = data.state.tolist()
 you_guessed = 0
 lives = 5
-answers=[]
+answers = []
 while lives > 0:
     answer = screen.textinput(title="guess the state", prompt=f"your total guess is {you_guessed}/50").title()
     if answer in all_states:
-        if answer not in answers:
-            check = data[data.state == answer]
-            new = Turtle("circle")
-            new.shapesize(0.2)
-            new.penup()
-            new.color("black")
-            new.goto(x=float(check.x), y=float(check.y))
-            new.write(answer)
-            answers.append(answer)
-            you_guessed += 1
+        all_states.remove(answer)
+        check = data[data.state == answer]
+        new = Turtle("circle")
+        new.shapesize(0.2)
+        new.penup()
+        new.color("black")
+        new.goto(x=float(check.x), y=float(check.y))
+        new.write(answer)
+        answers.append(answer)
+        you_guessed += 1
+    if answer == "Exit":
+        print(all_states)
+        break
+    if lives == 0:
+        print(all_states)
     else:
         lives -= 1
 screen.exitonclick()
